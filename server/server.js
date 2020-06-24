adb8798322c6db357a665722ccc540f362e5bf3e
Slava 4REAL <slava@4real.io>
2020-06-24 10:05:34 -0700

    sensible limits

--- server/server.js
+++ server/server.js
@@ -27,8 +27,8 @@ mongoose.Promise = global.Promise;
 const { validateTokenLenient, verify } = require('server/auth/auth.service');
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 1000 // limit each IP to 1000 requests per windowMs
 });
 
 console.log('NODE_ENV', process.env.NODE_ENV);
